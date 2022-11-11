e0117209376c8e6787e2c01a2018b21ffab0789b
pryority <matthewapryor@gmail.com>
2022-11-11 13:13:01 -0500

    update cli script

--- bin/cli.js
+++ bin/cli.js
@@ -14,13 +14,13 @@ const runCommand = (command) => {
 
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/pryority/nemiwind ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+const installDepsCommand = `cd ${repoName} && yarn install`;
 
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-console.log(`Installing dependencies for ${repoName}`);
+console.log(`💾 Installing dependencies for ${repoName}, your nemiwind app`);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exit(-1);
 
